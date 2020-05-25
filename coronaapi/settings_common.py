bad234ab7aaa66762328b6d52200e8286600f36a
Sergey Gavrilov <sergeigavrilov123@gmail.con>
2020-05-25 23:35:55 +0300

    change settings

--- coronaapi/settings_common.py
+++ coronaapi/settings_common.py
@@ -29,7 +29,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.sites',
 
     'rest_framework',
     'django_extensions',
