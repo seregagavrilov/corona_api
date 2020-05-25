9d239a5890dc26cb38df277cbd8f456e3ba0abe7
Sergey Gavrilov <sergeigavrilov123@gmail.con>
2020-05-25 16:36:11 +0300

    change settings

--- coronaapi/settings_prod_heroku.py
+++ coronaapi/settings_prod_heroku.py
@@ -45,8 +45,8 @@ STATICFILES_DIRS = (
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 MEDIA_ROOT = ''
-# AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
-# AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
+AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
 # AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
 # AWS_S3_HOST = os.environ['AWS_S3_HOST']
 # AWS_S3_ENDPOINT_URL = os.environ['AWS_S3_ENDPOINT_URL']
@@ -55,7 +55,7 @@ MEDIA_ROOT = ''
 # AWS_DEFAULT_ACL = os.environ['AWS_DEFAULT_ACL']
 
 # django-dbbackup
-DBBACKUP_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+# DBBACKUP_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 # DBBACKUP_STORAGE_OPTIONS = {
 #     'access_key': os.environ['S3_ACCESS_KEY'],
 #     'secret_key': os.environ['S3_SECRET_KEY'],
@@ -78,6 +78,4 @@ CHANNEL_LAYERS = {
     },
 }
 
-OSLA_API_ENDPOINT = "https://dev-api-osla.herokuapp.com"
-INTEGRATION_API_ENDPOINT = OSLA_API_ENDPOINT
 
