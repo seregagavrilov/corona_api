8b5cadb51d5d44387c3877168d0d5ef7d906f641
Sergey Gavrilov <sergeigavrilov123@gmail.con>
2020-05-28 17:27:26 +0300

    delete prints

--- apps/country/hendlers.py
+++ apps/country/hendlers.py
@@ -79,9 +79,6 @@ class StatisticLoader:
                 yield date, entry
 
     def update_timeline_data(self, current_indicator, current_amount, current_time):
-        print(self.current_country)
-        print(self.current_province)
-        print(current_time)
         CountryStatistic.objects.update_or_create(
             country=self.current_country,
             province=self.current_province,
